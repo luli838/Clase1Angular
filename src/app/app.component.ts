42c1171ad50ecd9bf6aef0dc12364c25b34015d1
luli838 <57195161+luli838@users.noreply.github.com>
2024-06-06 08:12:16 -0300

    Clase 1 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {women} from '../data/women.json';
+console.log(women)
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  //templateUrl: './app.component.html',
+  styleUrl:'./app.component.css',
+  template: `
+  <section>
+    <h1>Dentro del componente</h1>
+    <h2>{{title? title:"There is not title"}}</h2>
+    <p>El resultado de la suma es {{num + numB}}</p>
+    <br />
+    <hr />
+    <br />
+    @if(user){
+      <p>Welcome {{ user }}</p>
+    }@else {
+      <p>Singn in, please</p>
+    }
+    </section>
+    <section>
+    
+
+    </section>
+    
+    
+    `,
+   
+
+  //styles:`h1{color: violet}`,
+  
 })
 export class AppComponent {
-  title = 'playground';
+  title = '';
+  num = 10;
+  numB = 45;
+  user= '';
+  women=women;
 }
